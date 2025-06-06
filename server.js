d77a6dbfd9ee4fe5fcd4b37a435da08a9c9bc6f0
projcjdevs <cjcabatian5@gmail.com>
2025-06-06 18:24:06 +0800

    Added framework for dashboard, will change that in the next few commits

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static('public')); 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'lp.html'));
 });
 
 // AUTHENTICATION ROUTES
